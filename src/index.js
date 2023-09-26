ee5a63762e5f48853d5eeff94d4be2d808bf77fb
Daksh Bhatia <dakshbh@amdocs.com>
2023-09-26 14:37:38 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './pages/Layout';
+import Homepage from './pages/Homepage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Homepage />} />
+        {/* <Route path="/createUser" element={<CreateUser />} />
+        <Route path="/editUser/:user_id" element={<EditUser />} />
+         */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Button, Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+
+const Homepage = () => {
+
+    return(
+        <Stack spacing={3} alignItems={"center"}>
+            
+        </Stack>
+    )
+}
+
+export default Homepage;
